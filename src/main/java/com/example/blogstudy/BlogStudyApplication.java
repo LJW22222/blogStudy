65a4df533108aa9edada08c78acbc30f5c9272b9
이종원 <whddnjs5255@gmail.com>
2025-05-18 21:17:08 +0900

    first commit

---
+++ src/main/java/com/example/blogstudy/BlogStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogStudyApplication.class, args);
+    }
+
+}
